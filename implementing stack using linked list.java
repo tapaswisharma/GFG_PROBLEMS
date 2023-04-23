c7d377e4470607e38f10fdf7235fa1d705bc4a27
Tapaswi sharma <130048461+tapaswisharma@users.noreply.github.com>
2023-04-24 00:26:47 +0530

    Create implementing stack using linked list.java

---
+++ implementing stack using linked list.java
@@ -0,0 +1,44 @@
+class MyStack 
+{
+    // class StackNode {
+    //     int data;
+    //     StackNode next;
+    //     StackNode(int a) {
+    //         data = a;
+    //         next = null;
+    //     }
+    // }   
+    StackNode top;
+    
+    //Function to push an integer into the stack.
+    void push(int a) 
+    {
+          StackNode newNode = new StackNode (a);
+        if(top == null)
+        {
+            top = newNode ;
+            return ;
+        }
+        
+        newNode.next = top ;
+        top = newNode ;
+        
+        return ;
+    }
+    
+    //Function to remove an item from top of the stack.
+    int pop() 
+    {
+        if(top==null )
+        {
+            return -1 ;
+        }
+        
+      int res = top.data ;
+      top = top.next ;
+    
+     
+        
+        return res ;
+    }
+}
